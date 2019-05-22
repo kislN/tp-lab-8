8a72a97c0e47624141663e6e8d47b127c9e0a360
kislN <43805765+kislN@users.noreply.github.com>
2019-05-22 10:13:38 +0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -9,66 +9,67 @@
 
 using namespace std;
 
-const int NPREF = 2; // количество слов в префиксе
-const int MAXGEN = 1000; //объем на выходе
-typedef deque<string> prefix;          // очередь префиксов
+const int NPREF = 2; // РєРѕР»РёС‡РµСЃС‚РІРѕ СЃР»РѕРІ РІ РїСЂРµС„РёРєСЃРµ
+const int MAXGEN = 1000; //РѕР±СЉРµРј РЅР° РІС‹С…РѕРґРµ
+typedef deque<string> prefix;          // РѕС‡РµСЂРµРґСЊ РїСЂРµС„РёРєСЃРѕРІ
 
 int main() {
-	setlocale(LC_ALL, "RUS");
-	int n;
-	prefix pref;
-	vector<string> vec;
-	map<prefix, vector<string> > statetab; // префикс-суффиксы
-	string st;
-	ifstream fin("input.txt");
-	if (fin) {
-		for (int i = 0; i < NPREF; i++) {
-			fin >> st;
-			pref.push_back(st);
-		}
-		while (fin >> st) {
-			if (statetab.count(pref) == 0) {
-				vec.push_back(st);
-				statetab.insert(pair<prefix, vector<string>>(pref, vec));
-				vec.clear();
-			}
-			else {
-				statetab[pref].push_back(st);
-			}
-			pref.pop_front();
-			pref.push_back(st);
-		}
-	}
-	else {
-		cout << "File is not found" << endl;
-	}
-	fin.close();
-	pref.clear();
 
-	ifstream f("input.txt");
-	for (int i = 0; i < NPREF; i++) {
-		f >> st;
-		pref.push_back(st);
-	}
-	f.close();
+    int n;
+    prefix pref;
+    vector<string> vec;
+    map<prefix, vector<string> > statetab; // РїСЂРµС„РёРєСЃ-СЃСѓС„С„РёРєСЃС‹
+    string st;
+    ifstream fin("input.txt");
 
-	ofstream out("output.txt");
-	srand(time(0));
-	out << pref[0] << " " << pref[1];
-	for (int i = 0; i < MAXGEN - 2; i++) {
-		if (statetab.count(pref)) {
-			n = rand() % statetab[pref].size();
-			out << statetab[pref][n] << " ";
-			pref.push_back(statetab[pref][n]);
-			pref.pop_front();
-		}
-		else break;
-	}
-	out.close();
+    if (fin.is_open()) {
+        for (int i = 0; i < NPREF; i++) {
+            fin >> st;
+            pref.push_back(st);
+        }
+        while (fin >> st) {
+            if (statetab.count(pref) == 0) {
+                vec.push_back(st);
+                statetab.insert(pair<prefix, vector<string>>(pref, vec));
+                vec.clear();
+            }
+            else {
+                statetab[pref].push_back(st);
+            }
+            pref.pop_front();
+            pref.push_back(st);
+        }
+    }
+    else {
+        cout << "File is not found" << endl;
+    }
+    fin.close();
+    pref.clear();
 
+    ifstream f("input.txt");
+    for (int i = 0; i < NPREF; i++) {
+        f >> st;
+        pref.push_back(st);
+    }
+    f.close();
 
-	system("pause");
-	return 0;
+    ofstream out("output.txt");
+    srand(time(0));
+    out << pref[0] << " " << pref[1];
+    for (int i = 0; i < MAXGEN - 2; i++) {
+        if (statetab.count(pref)) {
+            n = rand() % statetab[pref].size();
+            out << statetab[pref][n] << " ";
+            if((i + 1) % 10 == 0) out << endl;
+            pref.push_back(statetab[pref][n]);
+            pref.pop_front();
+
+        }
+        else break;
+    }
+    out.close();
+
+    return 0;
 
 
 }
